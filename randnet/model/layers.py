a9dc7e1c1aaf4e06a36e0fb8cf0775fad93e803b
Philipp Eisen <philipp.eisen@king.com>
2019-04-26 16:53:10 +0200

    first implementation of randomly wired layer

--- randnet/model/layers.py
+++ randnet/model/layers.py
@@ -64,26 +64,82 @@ class RandomWiring(keras.layers.Layer):
                  channels,
                  stride,
                  random_graph_model,
+                 kernel_size=(3,3),
                  n=32,
                  k=4,
                  p=0.75,
-                 m=5    ,
+                 m=5,
                  seed=0,
                  **kwargs):
         self.channels = channels
         self.stride = stride
         self.random_graph_model = random_graph_model
+        self.kernel_size = kernel_size
         self.n = n
         self.k = k
         self.p = p
         self.seed = seed
 
         if random_graph_model == "ws":
-            self.random_graph = nx.connected_watts_strogatz_graph(n, k, p, seed=seed)
+            random_graph = nx.connected_watts_strogatz_graph(n, k, p, seed=seed)
         elif random_graph_model == "ba":
-            self.random_graph = nx.barabasi_albert_graph(n, m)
+            random_graph = nx.barabasi_albert_graph(n, m, seed=seed)
         elif random_graph_model == 'er':
-            self.random_graph = nx.erdos_renyi_graph(n, p, seed=seed)
-        self.random_graph = nx.DiGraph(self.random_graph.edges)
+            random_graph = nx.erdos_renyi_graph(n, p, seed=seed)
+        else:
+            raise ValueError('random_graph_model must be either "ws", "ba" or "er"')
+
+        self.dag = nx.DiGraph()
+        self.dag.add_edges_from(random_graph.edges)
+
+        self.dag_output_nodes = [node for node in self.dag.nodes if self.dag.out_degree[node] == 0]
+
+        assert nx.is_directed_acyclic_graph(self.dag)
 
         super(RandomWiring, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_node = Node(channels=self.channels, kernel_size=self.kernel_size)
+        self.nodes = [Node(channels=self.channels, kernel_size=self.kernel_size) for _ in range(self.n)]
+        self.output_node = Node(channels=self.channels, kernel_size=self.kernel_size)
+
+    def call(self, inputs, **kwargs):
+        node_outputs = [None for _ in range(self.n)]
+
+        x = self.input_node([inputs])
+        for node in nx.topological_sort(self.dag):
+            if self.dag.in_degree[node] == 0:
+                node_outputs[node] = self.nodes[node]([x])
+                continue
+
+            predecessor_outputs = []
+            for predecessor in self.dag.predecessors(node):
+                predecessor_outputs.append(node_outputs[predecessor])
+
+            node_outputs[node] = self.nodes[node](predecessor_outputs)
+
+        dag_outputs = []
+        for dag_output_node in self.dag_output_nodes:
+            dag_outputs.append(node_outputs[dag_output_node])
+
+        x = self.output_node(dag_outputs)
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return TensorShape((input_shape[0][0], input_shape[0][1], input_shape[0][2], self.channels))
+
+    def get_config(self):
+        base_config = super(RandomWiring, self).get_config()
+        base_config['channels'] = self.channels
+        base_config['stride'] = self.stride
+        base_config['kernel_size'] = self.kernel_size
+        base_config['random_graph_model'] = self.random_graph_model
+        base_config['n'] = self.n
+        base_config['k'] = self.k
+        base_config['p'] = self.p
+        base_config['seed'] = self.seed
+        return base_config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
--- randnet/model/randnet.py
+++ randnet/model/randnet.py
@@ -7,14 +7,12 @@ from tensorflow.python import keras
 class RandNet(keras.Model):
     def __init__(self, num_classes):
         super(RandNet, self).__init__(name='rand_net')
-        self.randwire_1 = RandomWiring(16, 2, "ws", n=32, p=0.75, k=4, m=0)
+        self.randwire_1 = RandomWiring(16, 2, "ws", n=32, p=0.75, k=4)
+        self.global_average_pool = keras.layers.GlobalAveragePooling2D()
+        self.classify = keras.layers.Dense(num_classes)
 
     def call(self, inputs, training=None, mask=None):
-        x0 = self.node_op_0([inputs])
-        x1_1 = self.node_op_1_1([x0])
-        x1_2 = self.node_op_1_2([x0])
-        x1_3 = self.node_op_1_3([x0])
-        x2 = self.node_op_2([x1_1, x1_2, x1_3])
-        out = self.global_average_pool(x2)
-        out = self.classify(out)
+        x = self.randwire_1(inputs)
+        x = self.global_average_pool(x)
+        out = self.classify(x)
         return out
